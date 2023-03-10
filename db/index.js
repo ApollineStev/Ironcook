e752b313ba8df552ed8c218b1579c3d76431320a
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-03-10 11:54:28 +0100

    data(user, recipe) created, type error(Recipe.model) edited

--- db/index.js
+++ db/index.js
@@ -2,6 +2,26 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
+const User = require("../models/User.model")
+const Recipe = require("../models/Recipe.model")
+
+const users = [
+  { username: "DiogoBarros", 
+    email: "diogo.barros@ironhack.com", 
+    passwordHash: "$2a$10$LZ8XiLY1melVKdAr.E/WreBSaO.W66uclyIuH3.7CgKqUhdP7FDua"
+  },
+]
+// Password for DiogoBarros -> DiogoBarros1
+
+const recipes = [
+  { // user: "", <- later needs populate
+    title: "French toast", 
+    description:" ", 
+    // ingredients, image, label
+  },
+]
+
+
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
@@ -13,6 +33,27 @@ mongoose
   .then((x) => {
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+
+    User.create(users)
+      .then(data => {
+
+        console.log(`${data.length} users inserted.`)
+        mongoose.connection.close()
+
+      }).catch((err) => {
+        console.error("Error creating products: ", err);
+      });
+
+    Recipe.create(recipes)
+      .then(data => {
+
+      console.log(`${data.length} recipes inserted.`)
+      mongoose.connection.close()
+
+      }).catch((err) => {
+      console.error("Error creating products: ", err);
+      });
+
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -30,4 +30,4 @@ const recipeSchema = new Schema(
   }
 );
 
-module.exports = model("User", userSchema);
+module.exports = model("Recipe", recipeSchema);
