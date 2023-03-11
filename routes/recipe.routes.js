95845abb0b00c1db0358882488288b76ebdc8804
ApollineStev <apollinecanard@gmail.com>
2023-03-11 14:50:19 +0100

    few layout changes

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js')
 
 const Recipe = require("../models/Recipe.model");
-
+const User = require("../models/User.model");
 
 // search a recipe (button click =>)
 /* router.get('/recipes/search', (req, res) => res.render('recipes/recipe-search.hbs'));
@@ -24,19 +24,31 @@ router.get('/recipes/search/results', (req, res) => {
 }); */
 
 // create a recipe
-router.get('/recipes/create', isLoggedIn, (req, res) => res.render('recipes/recipe-create.hbs'));
+router.get('/recipes/create', isLoggedIn, (req, res) => {
+    res.render('recipes/recipe-create.hbs')
+});
 
-router.post('/recipes/create', (req, res) => {
+router.post('/recipes/create', (req, res, next) => {
 
-    const { title, description, ingredients, cuisine, dishType, difficulty, timeToPrepare, image, date } = req.body;
+    const { user, title, description, ingredients, cuisine, dishType, difficulty, timeToPrepare, image, date } = req.body;
 
-    Recipe.create({ title, description, ingredients, cuisine, dishType, difficulty, timeToPrepare, image, date })
-    .then(() => res.redirect('/user-profile')) // how to render '/recipes/:recipeId'
+    Recipe.create({ user, title, description, ingredients, cuisine, dishType, difficulty, timeToPrepare, image, date })
+    .then(dbUser => {
+        return Recipe.findByIdAndUpdate( user, { $push: { user : dbUser._id } })
+    })
+    .then(() => res.redirect('/userProfile')) // how to render '/recipes/:recipeId'
     .catch(error => next(error));
 
 });
 
-
+router.get('/userRecipes', isLoggedIn, (req, res, next) => {
+    Recipe.find()
+    .then(dbUser => {
+        console.log(dbUser)
+    })
+    //.then(() => res.render('user/user-recipes'))
+    res.render('user/user-recipes')
+})
 
 
 
