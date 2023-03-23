18c8ebdb79b8afdb98f6b3483df610f77be8235c
ApollineStev <apollinecanard@gmail.com>
2023-03-23 20:58:26 +0100

    few changes

--- routes/aboutUs.routes.js
+++ routes/aboutUs.routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const mongoose = require("mongoose");
-
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js')
 
 /* GET home page */
-router.get("/aboutUs", (req, res, next) => { res.render("about-us") });
+router.get("/aboutUs", (req, res, next) => { res.render("about-us" , {userInSession: req.session.currentUser}) });
 
 module.exports = router;
